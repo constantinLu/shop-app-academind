bba8b4860fb6b1a577051ea653b6f55da3802827
gizet <lunguucatalin@gmail.com>
2021-10-21 18:36:43 +0300

    ACAS-259 - End of http module.

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,47 +4,71 @@ import 'package:flutter_complete_guide/widgets/app_drawer.dart';
 import 'package:flutter_complete_guide/widgets/order_item_widget.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
-  const OrdersScreen({Key key}) : super(key: key);
+// SEE 258 video
+// IF THE WIDGET NEEDS TO BE REBUILT AGAIN you need to change it back to stateful
+class OrdersScreen extends StatelessWidget {
 
   static const ROUTE_NAME = "/orders";
 
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  // 3rd example ! is to changing it back to a Stateless
 
   //same as didChangeDependency hack
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).getOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   Future.delayed(Duration.zero).then((_) async {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     await Provider.of<Orders>(context, listen: false).getOrders();
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+  //   super.initState();
+  // }
+
+  //VERSION II with listen false
+  // @override
+  // void initState() {
+  //   _isLoading = true;
+  //   Provider.of<Orders>(context, listen: false).getOrders().then((_) { //if listen false !!
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Orders"),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersProvider.orders.length,
-              itemBuilder: (ctx, i) => OrderItemWidget(ordersProvider.orders[i]),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).getOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              // ...
+              // Do error handling stuff
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItemWidget(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
