602905628515153d9920a115cd729b367a61228b
gizet <lunguucatalin@gmail.com>
2021-10-22 16:59:07 +0300

    ACAS-266 - Firebase auth - sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/auth.dart';
 import 'package:flutter_complete_guide/providers/cart.dart';
 import 'package:flutter_complete_guide/providers/orders.dart';
 import 'package:flutter_complete_guide/providers/products.dart';
+import 'package:flutter_complete_guide/screens/auth_screen.dart';
 import 'package:flutter_complete_guide/screens/cart_screen.dart';
 import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
 import 'package:flutter_complete_guide/screens/user_products_screen.dart';
 import 'package:flutter_complete_guide/widgets/product_detail_screen.dart';
 import 'package:provider/provider.dart';
-
-import 'screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,6 +18,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider.value(
           value: Products(),
         ),
@@ -29,14 +32,11 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
-        title: 'Iarmaroc',
+        title: 'Iarma',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.lightGreen,
-          accentColor: Colors.green,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewScreen(),
+        theme: ThemeData(primarySwatch: Colors.lightGreen, accentColor: Colors.white, fontFamily: 'Lato'),
+        //textTheme: TextTheme(headline7: TextStyle(fontSize: 5), bodyText1: TextStyle(fontSize: 22), bodyText2: TextStyle(fontSize: 500))),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.ROUTE_NAME: (ctx) => ProductDetailScreen(),
           CartScreen.ROUTE_NAME: (ctx) => CartScreen(),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signup(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBxsmmQPGeGhq4aoPkikeI4tlb4YWmwDL8');
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+}
